74062cc503a243f862de6e6f98b98c7b00bf0e80
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-02 17:45:51 +0900

    Service 구현

---
+++ Boot_5_Mysql/src/main/java/com/iu/b5/board/notice/NoticeService.java
@@ -0,0 +1,49 @@
+package com.iu.b5.board.notice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.iu.b5.board.BoardService;
+import com.iu.b5.board.BoardVO;
+
+@Service //DI , IOC
+public class NoticeService implements BoardService{
+	
+	@Autowired //DI
+	private NoticeMapper noticeMapper;
+	
+	@Override
+	public int setInsert(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.setInsert(boardVO);
+	}
+
+	@Override
+	public int setUpdate(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.setUpdate(boardVO);
+	}
+
+	@Override
+	public int setDelete(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.setDelete(boardVO);
+	}
+
+	@Override
+	public BoardVO getSelectOne(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.getSelectOne(boardVO);
+	}
+
+	@Override
+	public List<BoardVO> getSelectList() throws Exception {
+		// TODO Auto-generated method stub
+		return noticeMapper.getSelectList();
+	}
+	
+	
+
+}
