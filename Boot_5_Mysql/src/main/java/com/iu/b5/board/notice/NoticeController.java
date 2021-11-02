c9d578734ee012c54c9105d428e2731498e9fe8c
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-02 17:11:32 +0900

    방법1

---
+++ Boot_5_Mysql/src/main/java/com/iu/b5/board/notice/NoticeController.java
@@ -0,0 +1,35 @@
+package com.iu.b5.board.notice;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iu.b5.board.BoardVO;
+
+@RestController
+@RequestMapping("/notice/**")
+public class NoticeController {
+
+	@Autowired
+	private NoticeService noticeService;
+	
+	@GetMapping("insert")
+	public void setInsert() throws Exception{
+		
+	}
+	
+	@GetMapping("selectOne")
+	public BoardVO getSelectOne(HttpServletRequest request) throws Exception{
+		String num = request.getParameter("num");
+		int n = Integer.parseInt(num);
+		BoardVO boardVO = new BoardVO();
+		boardVO.setNum(n);
+		boardVO = noticeService.getSelectOne(boardVO);
+		
+		return boardVO;
+	}
+
+}
