d676428dd70fc9996c4cbb2a07673a82cf474264
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-02 17:45:38 +0900

    DB 객체

---
+++ Boot_5_Mysql/src/main/java/com/iu/b5/board/BoardVO.java
@@ -0,0 +1,52 @@
+package com.iu.b5.board;
+
+import java.sql.Date;
+
+public class BoardVO {
+	
+	private Integer num;
+	private String title;
+	private String contents;
+	private String writer;
+	private Integer hit;
+	private Date regDate;
+	
+	public Integer getNum() {
+		return num;
+	}
+	public void setNum(Integer num) {
+		this.num = num;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public Integer getHit() {
+		return hit;
+	}
+	public void setHit(Integer hit) {
+		this.hit = hit;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	
+
+}
---
+++ Boot_5_Mysql/src/main/java/com/iu/b5/board/notice/NoticeVO.java
@@ -0,0 +1,7 @@
+package com.iu.b5.board.notice;
+
+import com.iu.b5.board.BoardVO;
+
+public class NoticeVO extends BoardVO{
+
+}
