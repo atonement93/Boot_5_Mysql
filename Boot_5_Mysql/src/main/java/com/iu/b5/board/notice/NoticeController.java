d2c47372fb97d6186e1feb7c32e71837d29dc7ec
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-03 10:11:05 +0900

    컨트로러 mapping 작업

--- Boot_5_Mysql/src/main/java/com/iu/b5/board/notice/NoticeController.java
+++ Boot_5_Mysql/src/main/java/com/iu/b5/board/notice/NoticeController.java
@@ -3,7 +3,9 @@ package com.iu.b5.board.notice;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +19,32 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	@GetMapping("insert")
-	public void setInsert() throws Exception{
+	public String setInsert() throws Exception{
+		return "board/insert";
+	}
+	
+	@PostMapping("insert")
+	public String setInsert(BoardVO boardVO) throws Exception {
+		int result = noticeService.setInsert(boardVO);
+//		if(result>0) {
+//			
+//		}
+		return "redirect:./selectList";
+	}
+	
+	@GetMapping("update")
+	public String setUpdate(BoardVO boardVO, Model model) throws Exception{
+		boardVO = noticeService.getSelectOne(boardVO);
+		model.addAttribute("boardVO", boardVO);
 		
+		return "board/update";
+	}
+	
+	@PostMapping("update")
+	public String setUpdate(BoardVO boardVO) throws Exception{
+		int result = noticeService.setUpdate(boardVO);
+		
+		return "redirect:./selectList";
 	}
 	
 	@GetMapping("selectOne")
