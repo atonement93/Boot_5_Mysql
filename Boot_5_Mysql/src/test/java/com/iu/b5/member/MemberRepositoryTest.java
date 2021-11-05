ee2d04091e4b42e6db5de5bc5a34890cf619096c
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-05 10:40:11 +0900

    DB 연결 테스트

---
+++ Boot_5_Mysql/src/test/java/com/iu/b5/member/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.iu.b5.member;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberRepositoryTest {
+	
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@Test
+	void setFileInsertTest() throws Exception{
+		MemberFilesVO memberFilesVO = new MemberFilesVO();
+		memberFilesVO.setMember_id("uesr");
+		memberFilesVO.setFileName("userFile");
+		memberFilesVO.setOriName("userOriginal");
+		int result = memberRepository.setFileInsert(memberFilesVO);
+		assertEquals(1, result);
+	}
+	
+	//@Test
+	void setInsertTest() throws Exception {
+		MemberVO memberVO = new MemberVO();
+		memberVO.setId("user");
+		memberVO.setPw("user");
+		memberVO.setName("user");
+		memberVO.setEmail("user@email.com");
+		
+		int result = memberRepository.setInsert(memberVO);
+		
+		assertEquals(1, result);
+	}
+
+}
