ce153a7d5012a22e232baf552d9704a7e34ff74b
atonement93 <85914639+atonement93@users.noreply.github.com>
2021-11-05 10:42:46 +0900

    회원가입 기능 만들기

---
+++ Boot_5_Mysql/src/main/java/com/iu/b5/member/MemberFilesVO.java
@@ -0,0 +1,34 @@
+package com.iu.b5.member;
+
+public class MemberFilesVO {
+	
+	private Integer fileNum;
+	private String member_id;
+	private String fileName;
+	private String oriName;
+	
+	public Integer getFileNum() {
+		return fileNum;
+	}
+	public void setFileNum(Integer fileNum) {
+		this.fileNum = fileNum;
+	}
+	public String getMember_id() {
+		return member_id;
+	}
+	public void setMember_id(String member_id) {
+		this.member_id = member_id;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public String getOriName() {
+		return oriName;
+	}
+	public void setOriName(String oriName) {
+		this.oriName = oriName;
+	}
+}
---
+++ Boot_5_Mysql/src/main/java/com/iu/b5/member/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.iu.b5.member;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+//@Repository - Mapper가 있어 생략가능
+@Mapper
+public interface MemberRepository {
+	//회원가입
+	public int setInsert(MemberVO memberVO) throws Exception;
+	
+	//회원파일
+	public int setFileInsert (MemberFilesVO memberFilesVO) throws Exception;
+}
